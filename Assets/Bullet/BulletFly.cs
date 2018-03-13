dd0d0378cb472474c0d0377d2bd5f8ace3199c54
Shachar Oz <shachar.oz@gmail.com>
2018-03-13 16:03:39 +0200

    basic fire mechanism

---
+++ Assets/Bullet/BulletFly.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class BulletFly : MonoBehaviour {
+
+    public float speed = 10;
+
+
+    public float lifeTime = 10;
+    private float _startTime;
+
+
+    // Use this for initialization
+    void Start () {
+        _startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (Time.time - _startTime > lifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Player/Fire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    public Transform Gun;
+    public Transform BulletParent;
+    public GameObject BulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireNow();
+        }
+
+    }
+
+    private void FireNow()
+    {
+        Instantiate(BulletPrefab, Gun.transform.position,Gun.transform.rotation, BulletParent);
+      
+    }
+}
