67c9ad976b264acf1f0ea5778c3ee8fbbb46b5b1
Shachar Oz <shachar.oz@gmail.com>
2018-03-13 15:33:47 +0200

    step 2- player moves

---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float _speed;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //controlling the player up and down, left and right
+        Move();
+
+    }
+
+
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        }
+    }
+ }
