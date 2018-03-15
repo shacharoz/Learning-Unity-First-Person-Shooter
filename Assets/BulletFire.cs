10fa089eaaff620b1a10a5908409144751f352df
Shachar Oz <shachar.oz@gmail.com>
2018-03-15 12:56:22 +0200

    example scene

---
+++ Assets/BulletFire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour {
+
+    public float _speed;
+
+    public float lifetime = 5;
+    private float firsttime;
+
+
+    // Use this for initialization
+    void Start () {
+        firsttime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime *_speed);
+        if ( Time.time - firsttime > lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+        
+
+
+
+    }
+}
