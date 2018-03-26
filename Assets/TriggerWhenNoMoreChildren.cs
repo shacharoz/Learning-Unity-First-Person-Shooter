0257e6f22e59859132a0040af606d3bd47c8d9fe
Shachar Oz <shachar.oz@gmail.com>
2018-03-26 22:08:55 +0300

    end game when all targets destroyed
    also add UI menu system

---
+++ Assets/TriggerWhenNoMoreChildren.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerWhenNoMoreChildren : MonoBehaviour {
+
+    public UnityEvent NoMoreChildren;
+
+    private bool triggeredOnce;
+
+	// Use this for initialization
+	void Start () {
+        triggeredOnce = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!triggeredOnce && transform.childCount == 0)
+        {
+            NoMoreChildren.Invoke();
+            triggeredOnce = true;
+        }
+	}
+}
