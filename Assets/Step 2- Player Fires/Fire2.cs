a98186eeaa4534cc785b7b08de17ff1787a45bb6
Shachar Oz <shachar.oz@gmail.com>
2019-03-21 00:51:03 +0100

    restructure

--- Assets/Step 2- Player Fires/Fire2.cs
+++ Assets/Step 2- Player Fires/Fire2.cs
@@ -13,15 +13,19 @@ public class Fire2 : MonoBehaviour {
     public GameObject bulletPrefab;
     public Transform gunLocation;
 
+
+    public Vector3 OriginalKingPosition;
+
     // Update is called once per frame
     void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //make sound
-            if (gunLocation.GetComponent<AudioSource>()) gunLocation.GetComponent<AudioSource>().Play();
-
             //fire a bullet from the gun location (prefab)
             Instantiate(bulletPrefab, gunLocation.position, gunLocation.rotation);
+
+            //make sound
+            if (gunLocation.GetComponent<AudioSource>()) gunLocation.GetComponent<AudioSource>().Play();
+            
         }
         
 	}
